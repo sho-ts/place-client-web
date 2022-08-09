3de5fbfb2a08e66310dbf26eceb4294b84ab9544
tagawa shouta <sho.webfe@gmail.com>
2022-08-09 19:31:09 +0900

    next/imageのdomain追加

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx'],
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 }
 
 module.exports = nextConfig
