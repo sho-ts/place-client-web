ae927f2cc0c8d61358941dc7a5ae30b877aa2236
tagawa shouta <sho.webfe@gmail.com>
2022-08-09 14:37:37 +0900

    globalStyleの追加

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,18 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import 'the-new-css-reset';
+import styles from '@/styles/global.style';
+
+import { Fragment } from 'react';
+import { Global } from '@emotion/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Global styles={styles} />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
