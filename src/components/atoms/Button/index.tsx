c86ef171a78c8774c98e8b02496262b3065aa5ff
tagawa shouta <sho.webfe@gmail.com>
2022-08-10 03:38:17 +0900

    Buttonにhrefを渡したときにnext/linkでwrapする

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,20 +1,30 @@
 import type { FC } from 'react';
 import type { ButtonProps } from '@mui/material/Button';
 import { Button as MUIButton } from '@mui/material';
+import Link from 'next/link';
 import { css } from '@emotion/react';
 
 type ButtonPosition = 'left' | 'center' | 'right';
 
 type Props = ButtonProps & {
   position?: ButtonPosition;
+  href?: string;
 };
 
 const Button: FC<Props> = (baseProps) => {
-  const { position = 'left', ...props } = baseProps;
+  const { position = 'left', href, ...props } = baseProps;
 
   return (
     <div css={styles.base(position)}>
-      <MUIButton {...props} />
+      {href ? (
+        <Link passHref href={href}>
+          <a css={styles.link}>
+            <MUIButton {...props} />
+          </a>
+        </Link>
+      ) : (
+        <MUIButton {...props} />
+      )}
     </div>
   );
 };
@@ -23,6 +33,9 @@ const styles = {
   base: (position: ButtonPosition) => css`
     display: flex;
     justify-content: ${position};
+  `,
+  link: css`
+    text-decoration: none;
   `,
 };
 
