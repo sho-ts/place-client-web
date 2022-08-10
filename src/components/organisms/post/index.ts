a35dc0ef1e5343002b111236c88b3985c5ff413f
tagawa shouta <sho.webfe@gmail.com>
2022-08-11 06:22:32 +0900

    不要コンポーネント削除

--- src/components/organisms/post/index.ts
+++ src/components/organisms/post/index.ts
@@ -1,3 +1,2 @@
 export { default as PostComposeForm } from './PostComposeForm';
-export { default as PostSearchForm } from './PostSearchForm';
 export { default as PostsGrid } from './PostsGrid';
--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,10 +1,12 @@
 import type { FC, ReactNode } from 'react';
-import type { Breakpoint } from '@mui/system/createTheme';
+import type { Breakpoint, Theme } from '@mui/system/createTheme';
 import { useRouter } from 'next/router';
 import { useUserState } from '@/states';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import { Fragment } from 'react';
 import { Container as MUIContainer, styled } from '@mui/material';
-import { Header, BottomNavi } from '@/components/molecules';
+import { PCHeader, BottomNavi } from '@/components/molecules';
 import { GuestBottomNavi } from '@/components/organisms/guest';
 import { AuthProvider } from '@/providers';
 
@@ -17,12 +19,14 @@ type Props = {
 const omitRoutes = ['/login', '/register'];
 
 const Layout: FC<Props> = ({ children, maxW = 'md', auth }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('lg'));
   const [user] = useUserState();
   const router = useRouter();
 
   return (
     <Fragment>
-      <Header />
+      {matches && <PCHeader />}
       {auth ? (
         <AuthProvider>
           <Container maxWidth={maxW}>{children}</Container>
