699d3e6e9c0a8f1d2ae14b22ba48ad14ccb05026
tagawa shouta <sho.webfe@gmail.com>
2022-08-11 07:20:33 +0900

    headerの左右にelementを配置できるように

--- src/components/molecules/MobileTypographyHeader/index.tsx
+++ src/components/molecules/MobileTypographyHeader/index.tsx
@@ -1,22 +1,44 @@
 import type { FC, ReactNode } from 'react';
-import { Typography as MUITypography, styled } from '@mui/material';
+import { Typography as MUITypography, Box, styled } from '@mui/material';
 import { MobileBaseHeader } from '@/components/molecules';
 
 type Props = {
   children?: ReactNode;
+  renderRight?: () => JSX.Element;
+  renderLeft?: () => JSX.Element;
 };
 
-const MobileTypographyHeader: FC<Props> = ({ children }) => {
+const MobileTypographyHeader: FC<Props> = ({
+  children,
+  renderLeft,
+  renderRight,
+}) => {
   return (
     <MobileBaseHeader>
-      <Typography>{children}</Typography>
+      <Inner>
+        {renderLeft?.()}
+        <span />
+        <Typography>{children}</Typography>
+        {renderRight?.()}
+      </Inner>
     </MobileBaseHeader>
   );
 };
 
 const Typography = styled(MUITypography)(() => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   textAlign: 'center',
   fontWeight: 'bold',
+  width: '100%',
+}));
+
+const Inner = styled(Box)(() => ({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'space-between',
   width: '100%',
 }));
 
