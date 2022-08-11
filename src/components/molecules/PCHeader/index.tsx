2a2e84f42007e986b76c4ea6a420b554d1c2e319
tagawa shouta <sho.webfe@gmail.com>
2022-08-12 05:38:55 +0900

    headerに検索を追加

--- src/components/molecules/PCHeader/index.tsx
+++ src/components/molecules/PCHeader/index.tsx
@@ -1,8 +1,10 @@
 import type { FC } from 'react';
 import { useRouter } from 'next/router';
+import { usePostSearch } from '@/hooks';
 import {
   AppBar as MUIAppBar,
   Toolbar as MUIToolbar,
+  TextField,
   IconButton,
   Box,
   styled,
@@ -12,11 +14,25 @@ import { Logo } from '@/components/atoms';
 
 const PCHeader: FC = () => {
   const router = useRouter();
+  const { keyword, handleSubmit, changeKeywordValue } = usePostSearch();
 
   return (
     <AppBar>
       <Toolbar>
         <Logo />
+        <form
+          onSubmit={handleSubmit}
+          style={{ marginLeft: 'auto', width: 250 }}
+        >
+          <TextField
+            onChange={changeKeywordValue}
+            fullWidth
+            size="small"
+            label="検索"
+            variant="outlined"
+            value={keyword}
+          />
+        </form>
         <Icons>
           <IconButton onClick={() => router.push('/home')}>
             <Home sx={styles.icon} />
@@ -60,7 +76,7 @@ const Toolbar = styled(MUIToolbar)(() => ({
 const Icons = styled(Box)(() => ({
   display: 'flex',
   gap: 8,
-  marginLeft: 'auto',
+  marginLeft: 32,
 }));
 
 export default PCHeader;
