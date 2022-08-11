26c99729404883fc895c95f43e89149191510460
tagawa shouta <sho.webfe@gmail.com>
2022-08-12 05:38:29 +0900

    ロジックをカスタムフックに移動

--- src/components/organisms/exploer/ExploreHeader/index.tsx
+++ src/components/organisms/exploer/ExploreHeader/index.tsx
@@ -1,33 +1,21 @@
-import type { FC, FormEvent } from 'react';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import type { FC } from 'react';
+import { usePostSearch } from '@/hooks';
 import { TextField } from '@mui/material';
 import { MobileBaseHeader } from '@/components/molecules';
 
 const ExploerHeader: FC = () => {
-  const router = useRouter();
-  const [keyword, setKeyword] = useState<string>('');
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    router.replace(`${router.pathname}?keyword=${keyword}`);
-  };
-
-  useEffect(() => {
-    if (router.isReady) {
-      setKeyword((router.query.keyword as string | undefined) ?? '');
-    }
-  }, [router]);
+  const { keyword, handleSubmit, changeKeywordValue } = usePostSearch();
 
   return (
     <MobileBaseHeader>
       <form onSubmit={handleSubmit} style={{ width: '100%' }}>
         <TextField
-          onChange={(e) => setKeyword(e.target.value)}
+          onChange={changeKeywordValue}
           fullWidth
           size="small"
           label="検索"
           variant="outlined"
+          value={keyword}
         />
       </form>
     </MobileBaseHeader>
