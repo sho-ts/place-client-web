2b64199fa487250836ebe820e61bc9432a0710ce
tagawa shouta <sho.webfe@gmail.com>
2022-08-12 06:06:46 +0900

    getPost（モック）を追加

---
+++ src/repositories/post/get/getPost.ts
@@ -0,0 +1,23 @@
+import type { User } from '@/types/user';
+import type { Post } from '@/types/post';
+
+const getPost = async (postId: string): Promise<Post> => {
+  return new Promise<Post>((resolve) => {
+    const author: User = {
+      authId: 'auth|xxxx',
+      userId: 'dummy',
+      name: 'ダミーユーザー',
+      avatar: 'https://images.unsplash.com/photo-1658890636421-3d3caa3a52b0?q=60',
+    };
+
+    resolve({
+      postId,
+      author,
+      images: ['https://images.unsplash.com/photo-1658890636421-3d3caa3a52b0?q=60'],
+      content: 'テスト',
+      createdAt: '2022-01-01 00:00:00',
+    });
+  });
+};
+
+export default getPost;
