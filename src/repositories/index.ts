a945170c26ab5c96a6c8c42959d5f3211b5b2bf2
tagawa shouta <sho.webfe@gmail.com>
2022-08-13 22:47:30 +0900

    getHeadersを作成

---
+++ src/repositories/index.ts
@@ -0,0 +1,15 @@
+import { AuthService } from '@/services';
+
+const authService = new AuthService();
+
+export const getHeaders = async () => {
+  try {
+    const { accessToken } = await authService.getCurrentUser();
+
+    return {
+      Authentication: accessToken
+    };
+  } catch (error) {
+    return {};
+  }
+};
