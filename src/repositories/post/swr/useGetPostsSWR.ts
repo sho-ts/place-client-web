17fcc7839cfa63a75de9d7a17147abcf680e8c7b
tagawa shouta <sho.webfe@gmail.com>
2022-08-15 09:11:28 +0900

    limitを12に変更

--- src/repositories/post/swr/useGetPostsSWR.ts
+++ src/repositories/post/swr/useGetPostsSWR.ts
@@ -3,10 +3,10 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 const useGetPostsSWR = (search?: string | string[]) => {
-  const query = search ? `?s=${search}` : '';
+  const query = search ? `&s=${search}` : '';
 
   return useSWR<PostsItem[]>(
-    `${process.env.NEXT_PUBLIC_API_URL}/v1/post${query}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/v1/post?limit=12${query}`,
     (url) => axios.get(url).then((res) => res.data)
   );
 };
