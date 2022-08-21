f00595048d2997afa9b3b890261043d619cc347e
tagawa shouta <sho.webfe@gmail.com>
2022-08-22 05:52:26 +0900

    型を修正

--- src/utils/queryBuilder.ts
+++ src/utils/queryBuilder.ts
@@ -1,8 +1,15 @@
-const queryBuilder = (obj?: { [key: string]: string | number | string[] }) => {
-  if(!obj) return '';
+const queryBuilder = (obj?: {
+  [key: string]: string | number | string[] | undefined | null;
+}) => {
+  if (!obj) return '';
 
   const query = Object.entries(obj).reduce((acc, [key, value], index) => {
-    if (value === undefined || value === null || (typeof value === 'string' && value.length < 1)) return acc;
+    if (
+      value === undefined ||
+      value === null ||
+      (typeof value === 'string' && value.length < 1)
+    )
+      return acc;
 
     if (index !== 0) {
       return acc + `&${key}=${value}`;
