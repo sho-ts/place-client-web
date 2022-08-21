360f997f93c53bfc23c8b467d65392b987d9f60b
tagawa shouta <sho.webfe@gmail.com>
2022-08-22 05:49:47 +0900

    引数をnullableにする

--- src/utils/queryBuilder.ts
+++ src/utils/queryBuilder.ts
@@ -1,6 +1,9 @@
-const queryBuilder = (obj: { [key: string]: string | number }) => {
+const queryBuilder = (obj?: { [key: string]: string | number | string[] }) => {
+  if(!obj) return '';
+
   const query = Object.entries(obj).reduce((acc, [key, value], index) => {
     if (value === undefined || value === null || (typeof value === 'string' && value.length < 1)) return acc;
+
     if (index !== 0) {
       return acc + `&${key}=${value}`;
     }
