76355783a5c8ebee82decbfcde2d32ac61354b9e
tagawa shouta <sho.webfe@gmail.com>
2022-08-22 06:15:05 +0900

    spacingをpropsで変えられるようにする

--- src/components/organisms/post/PostsGrid/index.tsx
+++ src/components/organisms/post/PostsGrid/index.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import type { PostsFindAllResponse } from '@/types/response/post';
+import type { ResponsiveStyleValue } from '@/types/style';
 import { Box, styled } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import Image from 'next/image';
@@ -9,11 +10,12 @@ type Props = {
   posts: PostsFindAllResponse;
   xs?: number;
   md?: number;
+  spacing?: ResponsiveStyleValue<string | number>;
 };
 
-const PostsGrid: FC<Props> = ({ posts, xs, md }) => {
+const PostsGrid: FC<Props> = ({ posts, xs, md, spacing = 1 }) => {
   return (
-    <Grid container spacing={1}>
+    <Grid container spacing={spacing}>
       {posts.items.map(({ postId, thumbnail }) => (
         <Grid key={postId} xs={xs} md={md}>
           <Item>
---
+++ src/types/style.ts
@@ -0,0 +1,5 @@
+import { Breakpoint } from '@mui/system' 
+
+export type ResponsiveStyleValue<T> = T | Array<T | null> | {
+  [key in Breakpoint]?: T | null;
+};
