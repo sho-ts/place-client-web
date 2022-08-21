63560f7c855af7083961fd8c6a8419a09152293e
tagawa shouta <sho.webfe@gmail.com>
2022-08-22 05:44:37 +0900

    queryBuilderを追加

---
+++ src/utils/queryBuilder.ts
@@ -0,0 +1,13 @@
+const queryBuilder = (obj: { [key: string]: string | number }) => {
+  const query = Object.entries(obj).reduce((acc, [key, value], index) => {
+    if (value === undefined || value === null || (typeof value === 'string' && value.length < 1)) return acc;
+    if (index !== 0) {
+      return acc + `&${key}=${value}`;
+    }
+    return acc + `${key}=${value}`;
+  }, '');
+
+  return !!query ? `?${query}` : '';
+};
+
+export default queryBuilder;
