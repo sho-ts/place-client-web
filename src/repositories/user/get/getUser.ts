219e70bdff6562146efc8a53ae04fbe44d455044
tagawa shouta <sho.webfe@gmail.com>
2022-09-02 01:43:11 +0900

    ユーザー詳細のtypeを追加

--- src/repositories/user/get/getUser.ts
+++ src/repositories/user/get/getUser.ts
@@ -1,8 +1,8 @@
-import type { User } from '@/types/user';
+import type { UserDetail } from '@/types/user';
 import axios from 'axios';
 
 const getUser = async (userId: string) => {
-  return axios.get<User>(
+  return axios.get<UserDetail>(
     `${process.env.NEXT_PUBLIC_API_URL}/v1/users/${userId}`
   );
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,12 @@
+import { FOLLOW_STATUS } from '@/constants/follow';
+
 export type User = {
   userId: string;
   displayId: string;
   name: string;
   avatar: string;
+};
+
+export type UserDetail = User & {
+  followStatus: FOLLOW_STATUS;
 };
