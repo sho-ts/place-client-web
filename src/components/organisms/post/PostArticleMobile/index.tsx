3f6cdeb141caf5bfd5a4d1d8e45249dcc992ff25
tagawa shouta <sho.webfe@gmail.com>
2022-09-04 01:18:10 +0900

    投稿のコメント一覧（SP）を追加

--- src/components/organisms/post/PostArticleMobile/index.tsx
+++ src/components/organisms/post/PostArticleMobile/index.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react';
 import type { Post } from '@/types/post';
 import { styled } from '@mui/material/styles';
+import { useCallback } from 'react';
+import { useRouter } from 'next/router';
 import {
   PostAuthor,
   PostCaption,
@@ -9,7 +11,6 @@ import {
 } from '@/components/organisms/post';
 import { Container } from '@/components/templates';
 import Box from '@mui/material/Box';
-import MUIContainer from '@mui/material/Container';
 import { Fragment } from 'react';
 import Image from 'next/image';
 
@@ -18,6 +19,12 @@ type Props = {
 };
 
 const PostArticleMobile: FC<Props> = ({ post }) => {
+  const router = useRouter();
+
+  const handleCommentsButtonClick = useCallback(() => {
+    router.push(`/post/${post.postId}/comments`);
+  }, [post]);
+
   return (
     <Fragment>
       <Container>
@@ -27,7 +34,11 @@ const PostArticleMobile: FC<Props> = ({ post }) => {
         <Image src={post.files[0].url} layout="fill" objectFit="cover" />
       </ImageWrapper>
       <Container>
-        <PostIconButtons post={post} sx={{ mb: 1 }} />
+        <PostIconButtons
+          post={post}
+          sx={{ mb: 1 }}
+          handleCommentsButtonClick={handleCommentsButtonClick}
+        />
         <PostCaption post={post} />
         <PostCreatedAt createdAt={post.createdAt} sx={{ mt: 1 }} />
       </Container>
---
+++ src/pages/post/[postId]/comments/index.page.tsx
@@ -0,0 +1,105 @@
+import type { GetServerSideProps } from 'next';
+import type { NextPageWithLayout } from '@/types/page';
+import type { Post } from '@/types/post';
+
+import { getPost } from '@/repositories/post/get';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useGetPostCommentSWR } from '@/repositories/comment/swr';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import { TypographyHeader } from '@/components/molecules';
+import { PostComment, PostCommentForm } from '@/components/organisms/post';
+import { Layout, Wrapper, Container } from '@/components/templates';
+import { Fragment } from 'react';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIos from '@mui/icons-material/ArrowBackIos';
+
+type Props = {
+  post: Post;
+};
+
+const PostCommentsPage: NextPageWithLayout<Props> = ({ post }) => {
+  const matches = useMediaQuery('(max-width:700px)');
+  const router = useRouter();
+  const { data, mutate } = useGetPostCommentSWR(post.postId);
+
+  const caption = {
+    user: post.user,
+    content: post.caption,
+    createdAt: post.createdAt,
+  };
+
+  useEffect(() => {
+    // 特定のwindow幅以上で閲覧している場合投稿ページに戻す
+    if (!matches) router.push(`/post/${post.postId}`);
+  }, [matches]);
+
+  return (
+    <Fragment>
+      <TypographyHeader
+        renderLeft={() => (
+          <IconButton onClick={router.back}>
+            <ArrowBackIos sx={{ color: '#333' }} />
+          </IconButton>
+        )}
+      >
+        コメント
+      </TypographyHeader>
+      <Wrapper sx={{ pb: '136px' }}>
+        <Container>
+          <PostComment comment={caption} />
+          {data &&
+            data.items.map((comment) => (
+              <PostComment
+                sx={{ mt: 2 }}
+                key={comment.commentId}
+                comment={comment}
+              />
+            ))}
+          <PostCommentForm
+            sx={{
+              position: 'fixed',
+              bottom: '72px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+              width: 'calc(100% - 16px)',
+            }}
+            postId={post.postId}
+            mutate={mutate}
+          />
+        </Container>
+      </Wrapper>
+    </Fragment>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const postId = context.query.postId;
+
+  try {
+    if (!postId || typeof postId === 'object') {
+      throw new Error('投稿IDが不正です');
+    }
+
+    const { data } = await getPost(postId);
+
+    return {
+      props: {
+        post: data,
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+PostCommentsPage.getLayout = (page) => {
+  return <Layout>{page}</Layout>;
+};
+
+export default PostCommentsPage;
