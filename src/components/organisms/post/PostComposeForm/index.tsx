e0a7e6d6a817a578cf081584704bd3eb7d6208cc
tagawa shouta <sho.webfe@gmail.com>
2022-09-17 23:41:39 +0900

    画像の切り抜き機能を実装

--- src/components/organisms/post/PostComposeForm/index.tsx
+++ src/components/organisms/post/PostComposeForm/index.tsx
@@ -1,29 +1,62 @@
 import type { FC, ChangeEventHandler } from 'react';
 import { css } from '@emotion/react';
 import { styled } from '@mui/material/styles';
+import { ImageService } from '@/services';
 import { useRef, useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
+import { useModal } from '@/hooks';
 import { createPost } from '@/repositories/post/post';
 import { Button } from '@/components/atoms';
 import { TypographyHeader } from '@/components/molecules';
+import { PostCropImageModal } from '@/components/organisms/post';
 import { Container, Wrapper } from '@/components/templates';
 import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import AddCircleOutline from '@mui/icons-material/AddCircleOutline';
 import { Fragment } from 'react';
+import { Area } from 'react-easy-crop';
 
 const PostComposeForm: FC = () => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('lg'));
   const router = useRouter();
   const ref = useRef<HTMLInputElement>(null);
+  const [imageService] = useState(new ImageService());
   const [file, setFile] = useState<File | null>(null);
   const [objectUrl, setObjectUrl] = useState<string>('');
   const [caption, setCaption] = useState<string>('');
   const [areaKeyword, setAreaKeyword] = useState<string>('');
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+  const [croppedObject, setCroppedObject] = useState<Blob | null>(null);
+  const [croppedOjectUrl, setCroppedObjectUrl] = useState<string>('');
+
+  const onCropComplete = useCallback((_: any, croppedAreaPixels: Area) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const [isModalOpen, handleRequestOpen, handleRequestClose] = useModal();
+
+  /**
+   * 切り取り後の画像を生成し画面に表示
+   */
+  const showCroppedImage = useCallback(async () => {
+    if (!croppedAreaPixels) return;
+    try {
+      const croppedImage = await imageService.getCroppedImage(
+        objectUrl,
+        croppedAreaPixels
+      );
+      const croppedImageUrl = URL.createObjectURL(croppedImage);
+      setCroppedObject(croppedImage);
+      setCroppedObjectUrl(croppedImageUrl);
+      handleRequestClose();
+    } catch (e) {
+      console.error(e);
+    }
+  }, [croppedAreaPixels, objectUrl]);
 
   const handleOnClick = useCallback(() => {
     ref.current?.click();
@@ -35,15 +68,16 @@ const PostComposeForm: FC = () => {
     if (file) {
       setFile(file);
       setObjectUrl(window.URL.createObjectURL(file));
+      handleRequestOpen();
     }
   };
 
   const handleSubmit = useCallback(async () => {
-    if (!file) return;
+    if (!file || !croppedObject) return;
 
     try {
       const formData = new FormData();
-      formData.append('attachmentFile', file);
+      formData.append('attachmentFile', croppedObject);
       formData.append('caption', caption ?? '');
 
       await createPost(formData);
@@ -52,7 +86,7 @@ const PostComposeForm: FC = () => {
     } catch (error) {
       alert('アップロードに失敗しました');
     }
-  }, [file, caption]);
+  }, [file, caption, croppedObject]);
 
   return (
     <Fragment>
@@ -74,20 +108,28 @@ const PostComposeForm: FC = () => {
         <Container maxW="sm">
           <Fields>
             <input onChange={handleUpload} type="file" hidden ref={ref} />
-            {!file ? (
+            {!croppedOjectUrl ? (
               <Fragment>
                 <IconButton sx={{ mr: 1 }} onClick={handleOnClick} size="large">
                   <AddCircleOutline fontSize="inherit" />
                 </IconButton>
               </Fragment>
             ) : (
-              <div css={styles.thubmnail.base}>
-                <div
+              <div
+                css={css`
+                  width: 52px;
+                  height: 52px;
+                  margin-right: 8px;
+                  flex-shrink: 0;
+                `}
+              >
+                <img
+                  css={css`
+                    width: 100%;
+                    height: 100%;
+                  `}
                   onClick={handleOnClick}
-                  css={styles.thubmnail.inner}
-                  style={{
-                    backgroundImage: `url(${objectUrl})`,
-                  }}
+                  src={croppedOjectUrl}
                 />
               </div>
             )}
@@ -122,6 +164,13 @@ const PostComposeForm: FC = () => {
           )}
         </Container>
       </Wrapper>
+      <PostCropImageModal
+        isOpen={isModalOpen}
+        objectUrl={objectUrl}
+        onCropComplete={onCropComplete}
+        showCroppedImage={showCroppedImage}
+        handleRequestClose={handleRequestClose}
+      />
     </Fragment>
   );
 };
